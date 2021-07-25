a138ff4678c2e8a778b79123e8654d9f22a04509
MalakKhashoqji <malakkhashogji2550@gmail.com>
2021-07-25 13:40:46 +0300

    Modified comments

--- AppleHomeKit_LedExample.ino
+++ AppleHomeKit_LedExample.ino
@@ -25,6 +25,8 @@ extern "C" homekit_characteristic_t cha_switch_on;
 static uint32_t next_heap_millis = 0;
 
 #define PIN_SWITCH 2 // GPIO of D4 in ESP8266
+// This pin is either used from the simple LED connection
+// or for the 5v relay Module
 
 
 //Called when the switch value is changed by  Home APP
